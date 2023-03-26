9c38c70bb9e97a664b051bb666b41f0e4369ccef
reynbow domingo <reynbow_domingo@outlook.com>
2023-03-26 20:55:46 +0800

    configure swagger

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TEST.API.Integration v1"));
 }
 
 app.UseHttpsRedirection();
