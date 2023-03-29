7bdf62490066bf0592241aba951d80a6ac1080d0
rpdomingo <reynbowdomingo@gmail.com>
2023-03-29 22:59:40 +0800

    Edited in Ubuntu

--- Program.cs
+++ Program.cs
@@ -9,12 +9,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TEST.API.Integration v1"));
-// }
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Development.API.Integration v1"));
+} 
+else {
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevelopmentAz.API.Integration v1"));
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
